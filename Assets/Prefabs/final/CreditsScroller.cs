4e51cc97400f8547365b8b7aec6269e2475cb339
施亦恬 <yshi57846@gmail.com>
2026-05-06 22:18:30 +0800

    Add final subtitle animation

--- Assets/Prefabs/final/CreditsScroller.cs
+++ Assets/Prefabs/final/CreditsScroller.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CreditsScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("捲動設定")]
+    [Tooltip("數值越大，往上捲動的速度越快")]
+    public float scrollSpeed = 10f;
+
+    private RectTransform rectTransform;
+
     void Start()
     {
-        
+        // 取得 UI 的 RectTransform 元件
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 讓文字隨著時間往上移動 (Vector2.up 即 Y 軸正向)
+        rectTransform.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
     }
 }
